16d23ed7e1d7155a5005497ae8dbbb5efb5540da
Maurice Tadros <maurice.tadros@gmail.com>
2020-05-28 21:19:56 -0600

    Added new tasks

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,25 @@
 module.exports = {
-  stories: ["../packages/mars-theme/src/**/*.stories.(js|mdx)"],
+  stories: [
+    "../packages/mars-theme/src/**/*.(story|stories).(js|mdx)",
+    "../docs/**/*.(story|stories).(js|mdx)",
+  ],
   addons: [
     "@storybook/preset-typescript",
+    "@storybook/addon-knobs/register",
     "@storybook/addon-actions",
     "@storybook/addon-links",
     "@storybook/addon-docs",
-    "@storybook/addon-jest/register"
+    "@storybook/addon-jest/register",
+    "@storybook/addon-actions/register",
+    "@storybook/addon-a11y/register",
+    "@storybook/addon-viewport/register",
+    "@storybook/addon-backgrounds/register",
+    "@whitespace/storybook-addon-html/register",
   ],
-  webpackFinal: config => {
+  webpackFinal: (config) => {
     // Frontity components are not compiled (!) so we have to make sure we compile them ourselves:
-    const tsRule = config.module.rules.find(rule => /ts/.test(rule.test));
+    const tsRule = config.module.rules.find((rule) => /ts/.test(rule.test));
     tsRule.exclude = /node_modules\/(?!(@frontity\/components|@frontity\/error|@frontity\/connect)\/).*/;
     return config;
-  }
+  },
 };
---
+++ packages/mars-theme/src/components/MenuModal/MenuModal.js
@@ -0,0 +1,80 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "frontity";
+import Link from "../link";
+import styled from "@emotion/styled/macro";
+
+/**
+ *  Modal used to display the menu in mobile views.
+ */
+const MenuModal = ({ state }) => {
+  const { menu } = state.theme;
+  const isThereLinks = menu != null && menu.length > 0;
+
+  return (
+    <>
+      <MenuOverlay />
+      <MenuContent as="nav">
+        {isThereLinks &&
+          menu.map(([name, link]) => (
+            <MenuLink
+              key={name}
+              link={link}
+              aria-current={state.router.link === link ? "page" : undefined}
+            >
+              {name}
+            </MenuLink>
+          ))}
+      </MenuContent>
+    </>
+  );
+};
+
+const MenuOverlay = styled.div`
+  background-color: #1f38c5;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden auto;
+  position: fixed;
+  z-index: -1;
+  top: 0;
+  left: 0;
+`;
+
+const MenuContent = styled.div`
+  z-index: 3;
+`;
+
+const MenuLink = styled(Link)`
+  width: 100%;
+  display: inline-block;
+  outline: 0;
+  font-size: 20px;
+  text-align: center;
+  padding: 1.2rem 0;
+
+  /* Styles added only for storybook.*/
+  text-decoration: none;
+  color: white;
+  /* Styles added only for storybook.*/
+
+  &:hover,
+  &:focus {
+    background-color: rgba(0, 0, 0, 0.05);
+  }
+  /* styles for active link */
+  &[aria-current="page"] {
+    color: yellow;
+    font-weight: bold;
+    /* border-bottom: 4px solid yellow; */
+  }
+`;
+
+MenuModal.propTypes = {
+  /**
+   * Frontity injected state: https://docs.frontity.org/learning-frontity/state
+   */
+  state: PropTypes.object
+};
+
+export default connect(MenuModal);
